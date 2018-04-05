2342ad538dc73551a7f37698a8fe8ba28665a9bc
David Staehler <david.staehler@googlemail.com>
2018-04-05 20:06:09 +1200

    Update youtube script to pull all videos in all playlists

--- youtube_service.rb
+++ youtube_service.rb
@@ -17,8 +17,31 @@ def fetch_video_data(channel)
   end
 end
 
+def fetch_playlist_data(channel)
+  channel.playlists.each do |playlist|
+    FileUtils.mkdir_p("_data/youtube_playlists/#{playlist.id}")
+    playlist.playlist_items.each do |item|
+      playlist_video = {
+        playlist_item_id: item.id,
+        item_title: item.title,
+        description: item.description,
+        published_at: item.published_at,
+        thumbnail_url: item.thumbnail_url,
+        channel_id: item.channel_id,
+        channel_title: item.channel_title,
+        playlist_id: item.playlist_id,
+        video_id: item.video_id,
+        position: item.position
+      }
+
+      File.open("_data/youtube_playlists/#{item.playlist_id}/#{item.id}.yml", 'w') {|f| f.write playlist_video.to_yaml }
+    end
+  end
+end
+
 require 'yt'
 require 'yaml'
+require 'fileutils'
 
 Yt.configure do |config|
   config.api_key = API_KEY
@@ -26,3 +49,4 @@ end
 
 channel = Yt::Channel.new(id: CHANNEL_ID)
 fetch_video_data(channel)
+fetch_playlist_data(channel)
