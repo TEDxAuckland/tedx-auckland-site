13d47173decd7fa0c0656fe95858a6f37d70554c
David Staehler <david.staehler@googlemail.com>
2018-09-30 16:40:57 +1300

    Add 2018 event countdown

---
+++ src/assets/js/countdown.js
@@ -0,0 +1,34 @@
+<!-- Display the countdown timer in an element -->
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 13, 2018 09:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+  var countdown = document.getElementById("countdown")
+
+  if (countdown)  {
+    countdown.innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      countdown.innerHTML = "Happening now!";
+    }
+  }
+  // Display the result in the element with id="countdown"
+}, 1000);
